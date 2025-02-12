e4f1fec2afa0e5abeeb35e8e8469064429f222a3
Augusto fragg <augustofragg@gmail.com>
2025-02-11 21:52:30 -0300

    login system features

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -5,29 +5,42 @@ use \src\models\User;
 
 class LoginHandler {
 
-     public static function checkLogin()
-     {
-        if(!empty($_SESSION['token']))
+        public static function checkLogin()
         {
-            $token = $_SESSION['token'];
+            if(!empty($_SESSION['token']))
+            {
+                $token = $_SESSION['token'];
 
-            $data = User::select()->where('token', $token)->one();    
-            if(count($data) > 0) 
-            {
-                $loggedUser = new User();
-                $loggedUser->setId($data['id']);
-                $loggedUser->setEmail($data['email']);
-                $loggedUser->setName($data['name']);
-                
-                return $loggedUser;
-            }
-            else 
-            {
-                return false;
+                $data = User::select()->where('token', $token)->one();    
+                if(count($data) > 0) 
+                {
+                    $loggedUser = new User();
+                    $loggedUser->setId($data['id']);
+                    $loggedUser->setEmail($data['email']);
+                    $loggedUser->setName($data['name']);
+                    
+                    return $loggedUser;
+                }
             }
         }
 
-        return false;
-     }   
+        public static function verifyLogin($email, $password) {
+            $user = User::select()->where('email',$email)->one();
 
+            if($user) {
+                if (password_verify($password,$user['passoword'])) {
+                    
+                    $token = md5(time().rand(0,9999).time());
+
+                    User::update()
+                        ->set('token',$token)
+                        ->where('email',$email)
+                    ->execute();
+
+                    return $token;
+                }
+            }
+            return false;       
+        }
+      
 }
